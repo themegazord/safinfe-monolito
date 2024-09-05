d7504be77a4fd49c8c690f9da73bee9530b5bb5e
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2024-09-05 09:19:32 -0400

    chore: adicionado validacao manual para verificar campos existentes no bd na tabela de empresas

--- app/Livewire/Views/Empresas/Cadastro.php
+++ app/Livewire/Views/Empresas/Cadastro.php
@@ -6,6 +6,7 @@ use App\Livewire\Forms\EmpresaForm;
 use App\Livewire\Forms\EnderecoForm;
 use App\Repositories\Eloquent\Repository\EmpresaRepository;
 use App\Repositories\Eloquent\Repository\EnderecoRepository;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
@@ -27,8 +28,13 @@ class Cadastro extends Component
     redirect('/empresas');
   }
 
-  public function cadastrar(EmpresaRepository $empresaRepository, EnderecoRepository $enderecoRepository): void {
+  public function cadastrar(EmpresaRepository $empresaRepository, EnderecoRepository $enderecoRepository) {
     $this->endereco->tratarCamposSujos();
+
+    if (!is_null(DB::table('empresas')->where('cnpj', $this->empresa->cnpj)->first())) return $this->addError('empresa.cnpj', 'CNPJ já existente.');
+    if (!is_null(DB::table('empresas')->where('ie', $this->empresa->ie)->first())) return $this->addError('empresa.ie', 'IE já existente.');
+    if (!is_null(DB::table('empresas')->where('email_contato', $this->empresa->email_contato)->first())) return $this->addError('empresa.email_contato', 'Email de contato já existente.');
+
     $this->endereco->validate();
 
     $endereco_cadastrado = $enderecoRepository->cadastraEndereco($this->endereco->all());
