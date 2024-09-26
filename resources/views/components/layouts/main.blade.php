17eeb4687f9606e23e555b33b6993f15aa2a3d52
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2024-09-26 14:42:33 -0400

    fix: nome do componente de navbar

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -41,7 +41,7 @@
 </head>
 
 <body>
-  <livewire:componentes.utils.navbar.main-nav-bar />
+  <livewire:componentes.utils.navbar.main-navbar />
   <livewire:componentes.utils.notificacao.flash />
   {{ $slot }}
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
