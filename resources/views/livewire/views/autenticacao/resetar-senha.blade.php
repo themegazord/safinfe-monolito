a4d38fdd9ec7b9fc210589bc8b0b8948bada2f4d
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-04-14 18:34:09 -0400

    refact: ui da tela de solicitacao do reset de senha

--- resources/views/livewire/views/autenticacao/resetar-senha.blade.php
+++ resources/views/livewire/views/autenticacao/resetar-senha.blade.php
@@ -1,15 +1,8 @@
 <form wire:submit="alterarSenha" class="container justify-center items-center w-[90%] sm:w-[50%] rounded-lg p-4 bg-gray-600">
   <h1 class="font-bold text-2xl">Insira os dados para resetar a senha.</h1>
   <div class="mb-4">
-    <x-input label="Insira seu email" wire:model="resetSenha.email" placeholder="Insira seu email..." inline/>
+    <x-input label="Insira o email da conta que deseja alterar a senha" wire:model="resetSenha.email" placeholder="Insira seu email..." inline/>
   </div>
-  <div class="mb-4">
-    <x-password label="Insira sua senha antiga" wire:model="resetSenha.oldPassword" placeholder="Insira sua senha antiga..." inline clearable/>
-  </div>
-  <div class="mb-4">
-    <x-password label="Insira sua senha nova" wire:model="resetSenha.newPassword" placeholder="Insira sua senha nova..." inline clearable/>
-  </div>
-  <div class="flex w-full">
-    <x-button class="btn btn-success mx-auto" label="Alterar senha" spinner="alterarSenha" wire:click="Alterar senha"/>
-  </div>
+
+  <x-button class="btn btn-primary" label="Enviar email" wire:click="alterarSenha"/>
 </form>
