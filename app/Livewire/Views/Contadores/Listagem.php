ffce3b1b3d785f102d67a002abcf3a2fa828eb98
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-04-15 10:09:48 -0400

    chore: limpado rotina de listagem de contadores

--- app/Livewire/Views/Contadores/Listagem.php
+++ app/Livewire/Views/Contadores/Listagem.php
@@ -3,10 +3,7 @@
 namespace App\Livewire\Views\Contadores;
 
 use App\Models\Contador;
-use App\Repositories\Eloquent\Repository\ContadorRepository;
-use Illuminate\Support\Facades\Session;
 use Livewire\Attributes\Layout;
-use Livewire\Attributes\On;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 use Livewire\WithoutUrlPagination;
@@ -26,29 +23,13 @@ class Listagem extends Component
 
   #[Title('SAFI NFE - Listagem de Contadores')]
   #[Layout('components.layouts.main')]
-  public function render(ContadorRepository $contadorRepository)
+  public function render()
   {
-    $contadores = $contadorRepository->paginacaoContadores(10, $this->pesquisa, $this->estaAtivo);
-    return view('livewire.views.contadores.listagem', [
-      'listagem' => compact('contadores')
-    ]);
+    return view('livewire.views.contadores.listagem');
   }
 
   public function irEdicaoContador(int $contador_id): void {
     redirect("/contadores/edicao/$contador_id");
-  }
-
-  #[On('inativar-contador')]
-  public function alteraStatusContador(int $contador_id): void {
-    $contador = new Contador();
-    if ($contador->onlyTrashed()->where('contador_id', $contador_id)->first()) {
-      $contador->onlyTrashed()->where('contador_id', $contador_id)->first()->restore();
-      Session::flash('sucesso', 'Contador ativado com sucesso');
-    } else {
-      $contador->where('contador_id', $contador_id)->first()->delete();
-      Session::flash('sucesso', 'Contador inativado com sucesso');
-    }
-    redirect('contadores/');
   }
 
   public function irCadastrar(): void {
