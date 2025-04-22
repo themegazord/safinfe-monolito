df0ba00970df2d6070b6a124322370897da2d534
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-04-22 14:09:32 -0400

    refact: refatorado codigo de recebimento de notas fiscais pela api

--- resources/views/livewire/views/consultaxml/consulta.blade.php
+++ resources/views/livewire/views/consultaxml/consulta.blade.php
@@ -26,77 +26,78 @@
   @endif
 
   @php
-  $headers = [
-  ['key' => 'dados_id', 'label' => 'ID'],
-  ['key' => 'modelo', 'label' => 'Modelo'],
-  ['key' => 'serie', 'label' => 'Série'],
-  ['key' => 'numeronf', 'label' => 'Número NF'],
-  ['key' => 'numeronf_final', 'label' => 'Número NF Final'],
-  ['key' => 'status', 'label' => 'Status'],
-  ['key' => 'dh_emissao_evento', 'label' => 'Data do evento'],
-  ];
+    $headers = [
+      ['key' => 'dados_id', 'label' => 'ID'],
+      ['key' => 'modelo', 'label' => 'Modelo'],
+      ['key' => 'serie', 'label' => 'Série'],
+      ['key' => 'numeronf', 'label' => 'Número NF'],
+      ['key' => 'numeronf_final', 'label' => 'Número NF Final'],
+      ['key' => 'status', 'label' => 'Status'],
+      ['key' => 'dh_emissao_evento', 'label' => 'Data do evento'],
+    ];
 
-  $dados_xml = DB::table('dados_xml as dx1')
-  ->where(function ($query) {
-  $query->whereIn('dx1.status', ['cancelado', 'denegado', 'inutilizado'])
-  ->orWhere(function ($subQuery) {
-  $subQuery->where('dx1.status', 'autorizado')
-  ->whereNotExists(function ($subSubQuery) {
-  $subSubQuery->select(DB::raw(1))
-  ->from('dados_xml as dx2')
-  ->whereRaw('dx2.numeronf = dx1.numeronf')
-  ->whereIn('dx2.status', ['cancelado', 'denegado', 'inutilizado']);
-  });
-  });
-  })
-  ->where('empresa_id', $dadosXML['empresa_id'])
+    $dados_xml = DB::table('dados_xml as dx1')
+      ->where(function ($query) {
+        $query->whereIn('dx1.status', ['cancelado', 'denegado', 'inutilizado'])
+          ->orWhere(function ($subQuery) {
+            $subQuery->where('dx1.status', 'autorizado')
+              ->whereNotExists(function ($subSubQuery) {
+                $subSubQuery->select(DB::raw(1))
+                  ->from('dados_xml as dx2')
+                  ->whereRaw('dx2.numeronf = dx1.numeronf')
+                  ->whereIn('dx2.status', ['cancelado', 'denegado', 'inutilizado']);
+              });
+          });
+      })
+      ->where('empresa_id', $dadosXML['empresa_id'])
 
-  // filtro de data com when
-  ->when(!is_null($dadosXML['data_inicio']) && !is_null($dadosXML['data_fim']), function ($query) use ($dadosXML) {
-  $inicio = date('Y-m-d', strtotime($dadosXML['data_inicio']));
-  $fim = date('Y-m-d', strtotime($dadosXML['data_fim']));
+      // filtro de data com when
+      ->when(!is_null($dadosXML['data_inicio']) && !is_null($dadosXML['data_fim']), function ($query) use ($dadosXML) {
+        $inicio = date('Y-m-d', strtotime($dadosXML['data_inicio']));
+        $fim = date('Y-m-d', strtotime($dadosXML['data_fim']));
 
-  if ($inicio === $fim) {
-  $query->whereDate('dh_emissao_evento', $inicio);
-  } else {
-  $query->whereBetween(DB::raw('DATE(dh_emissao_evento)'), [$inicio, $fim]);
-  }
-  })
-  ->when(!is_null($dadosXML['data_inicio']) && is_null($dadosXML['data_fim']), function ($query) use ($dadosXML) {
-  $query->whereDate('dh_emissao_evento', '>=', date('Y-m-d', strtotime($dadosXML['data_inicio'])));
-  })
-  ->when(is_null($dadosXML['data_inicio']) && !is_null($dadosXML['data_fim']), function ($query) use ($dadosXML) {
-  $query->whereDate('dh_emissao_evento', '<=', date('Y-m-d', strtotime($dadosXML['data_fim'])));
-    })
+        if ($inicio === $fim) {
+          $query->whereDate('dh_emissao_evento', $inicio);
+        } else {
+          $query->whereBetween(DB::raw('DATE(dh_emissao_evento)'), [$inicio, $fim]);
+        }
+      })
+      ->when(!is_null($dadosXML['data_inicio']) && is_null($dadosXML['data_fim']), function ($query) use ($dadosXML) {
+        $query->whereDate('dh_emissao_evento', '>=', date('Y-m-d', strtotime($dadosXML['data_inicio'])));
+      })
+      ->when(is_null($dadosXML['data_inicio']) && !is_null($dadosXML['data_fim']), function ($query) use ($dadosXML) {
+        $query->whereDate('dh_emissao_evento', '<=', date('Y-m-d', strtotime($dadosXML['data_fim'])));
+      })
 
-    // status
-    ->when($dadosXML['status'] !== "TODAS", function ($query) use ($dadosXML) {
-    $query->where('dx1.status', $dadosXML['status']);
-    })
+      // status
+      ->when($dadosXML['status'] !== "TODAS", function ($query) use ($dadosXML) {
+        $query->where('dx1.status', $dadosXML['status']);
+      })
 
-    // modelo
-    ->when($dadosXML['modelo'] !== "TODAS", function ($query) use ($dadosXML) {
-    $query->where('dx1.modelo', $dadosXML['modelo']);
-    })
+      // modelo
+      ->when($dadosXML['modelo'] !== "TODAS", function ($query) use ($dadosXML) {
+        $query->where('dx1.modelo', $dadosXML['modelo']);
+      })
 
-    // série
-    ->when(!is_null($dadosXML['serie']), function ($query) use ($dadosXML) {
-    $query->where('dx1.serie', $dadosXML['serie']);
-    })
+      // série
+      ->when(!is_null($dadosXML['serie']), function ($query) use ($dadosXML) {
+        $query->where('dx1.serie', $dadosXML['serie']);
+      })
 
-    // faixa de número NF
-    ->when(!is_null($dadosXML['numeroInicial']) && !is_null($dadosXML['numeroFinal']), function ($query) use ($dadosXML) {
-    $query->whereBetween('numeronf', [$dadosXML['numeroInicial'], $dadosXML['numeroFinal']]);
-    })
-    ->when(!is_null($dadosXML['numeroInicial']) && is_null($dadosXML['numeroFinal']), function ($query) use ($dadosXML) {
-    $query->where('numeronf', '>=', $dadosXML['numeroInicial']);
-    })
-    ->when(is_null($dadosXML['numeroInicial']) && !is_null($dadosXML['numeroFinal']), function ($query) use ($dadosXML) {
-    $query->where('numeronf', '<=', $dadosXML['numeroFinal']);
+      // faixa de número NF
+      ->when(!is_null($dadosXML['numeroInicial']) && !is_null($dadosXML['numeroFinal']), function ($query) use ($dadosXML) {
+        $query->whereBetween('numeronf', [$dadosXML['numeroInicial'], $dadosXML['numeroFinal']]);
+      })
+      ->when(!is_null($dadosXML['numeroInicial']) && is_null($dadosXML['numeroFinal']), function ($query) use ($dadosXML) {
+        $query->where('numeronf', '>=', $dadosXML['numeroInicial']);
+      })
+      ->when(is_null($dadosXML['numeroInicial']) && !is_null($dadosXML['numeroFinal']), function ($query) use ($dadosXML) {
+        $query->where('numeronf', '<=', $dadosXML['numeroFinal']);
       });
 
-      $dados_xml=$dados_xml->orderBy('dx1.dados_id')->paginate($this->porPagina);
-      @endphp
+    $dados_xml = $dados_xml->orderBy('dx1.dados_id')->paginate($this->porPagina);
+  @endphp
+
 
       <x-table
         :headers="$headers"
