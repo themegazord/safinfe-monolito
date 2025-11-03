9ad764727cdbf0cf83b0c2c49d50fdcb48803c77
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-11-03 15:53:40 -0400

    feat(backup): implementa estratégia de backup automático e restauração
    - Configura agendamento diário de backups (01:00) e limpeza (02:00)
    - Adiciona script restore-database.sh para restauração de backups
    - Desabilita backup de arquivos no config/backup.php (somente database)
    - Adiciona comando 'composer format' para executar Pint
    - Ajusta indent_size de 2 para 4 espaços no .editorconfig
    - Remove arquivo pint-watch.sh (substituído por comando composer)
    - Remove comando inspire do console (não utilizado)

--- config/backup.php
+++ config/backup.php
@@ -14,9 +14,7 @@ return [
                 /*
                  * The list of directories and files that will be included in the backup.
                  */
-                'include' => [
-                    base_path(),
-                ],
+                'include' => [],
 
                 /*
                  * These directories and files will be excluded from the backup.
